2793836e6054703004f86097d588bcdfc665d953
shiva <shiva_05@outlook.com>
2025-06-30 21:51:21 +0530

    chore:improved description

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,8 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "stackd",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "STACKD - Full Stack Project Generator",
+	Long: "STACKD is a CLI tool to scaffold full stack projects with various configurations.It supports multiple front-end and back-end frameworks and  database configurations",
 }
 
 func Execute() {
