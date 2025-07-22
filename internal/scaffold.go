15a4e7c5db9f4f6d4fa477636460c82f9e055beb
shyam sunder v <shyam@Mac.mshome.net>
2025-07-22 14:04:24 +0530

    added concurrency

--- internal/scaffold.go
+++ internal/scaffold.go
@@ -1,11 +1,13 @@
 package internal
 
 import (
+	"sync"
+
 	executors_fb "github.com/shivasankaran18/STACKD/internal/fb/executorsfb"
-	executors_fullstack "github.com/shivasankaran18/STACKD/internal/fs/executorsfs"
-	executors_monorepo "github.com/shivasankaran18/STACKD/internal/monorepo/executorsmr"
 	prompt_fb "github.com/shivasankaran18/STACKD/internal/fb/promptfb"
+	executors_fullstack "github.com/shivasankaran18/STACKD/internal/fs/executorsfs"
 	prompt_fullstack "github.com/shivasankaran18/STACKD/internal/fs/promptfs"
+	executors_monorepo "github.com/shivasankaran18/STACKD/internal/monorepo/executorsmr"
 	prompt_monorepo "github.com/shivasankaran18/STACKD/internal/monorepo/promptmr"
 	"github.com/shivasankaran18/STACKD/internal/utils"
 )
@@ -25,10 +27,9 @@ func Scaffold() {
 		if frontend != prompt_fb.Frontend_None {
 			ui = prompt_fb.AskUI()
 		}
-
 		backend = prompt_fb.AskBackend()
 		if backend != prompt_fb.Backend_None {
-			orm := utils.AskORM()
+			orm = utils.AskORM()
 			if orm != utils.Orms_None {
 				dbType = utils.AskDatabaseType()
 				dbURL = utils.AskDatabaseURL(dbType)
@@ -37,19 +38,26 @@ func Scaffold() {
 		}
 
 		utils.CreateDirectories(dir)
+		var wg sync.WaitGroup
+		wg.Add(2)
 		go func() {
+			defer wg.Done()
 			executors_fb.CreateFrontend(dir, frontend)
 			if frontend != prompt_fb.Frontend_None {
 				executors_fb.CreateUI(dir, ui, frontend)
 			}
 		}()
-		executors_fb.CreateBackend(dir, backend)
-		if backend != prompt_fb.Backend_None && orm != utils.Orms_None {
-			executors_fb.CreateORM(dir, orm, dbURL, dbType)
-		}
-		if backend != prompt_fb.Backend_None {
-			executors_fb.CreateAuth(dir, auth, backend)
-		}
+		go func() {
+			defer wg.Done()
+			executors_fb.CreateBackend(dir, backend)
+			if backend != prompt_fb.Backend_None && orm != utils.Orms_None {
+				executors_fb.CreateORM(dir, orm, dbURL, dbType)
+			}
+			if backend != prompt_fb.Backend_None {
+				executors_fb.CreateAuth(dir, auth, backend)
+			}
+		}()
+		wg.Wait()
 
 	} else if projType == utils.ProjectTypeFullStack {
 		fullStack := prompt_fullstack.AskFullStack()
@@ -57,22 +65,31 @@ func Scaffold() {
 		orm := utils.AskORM()
 		var dbType utils.DbTypeResponse
 		var dbURL string
-		if(orm != utils.Orms_None) {
+		if orm != utils.Orms_None {
 			dbType = utils.AskDatabaseType()
-				dbURL=utils.AskDatabaseURL(dbType)
-
-		} 
+			dbURL = utils.AskDatabaseURL(dbType)
+		}
 
 		utils.CreateDirectories(dir)
-		executors_fullstack.CreateFullStack(dir, fullStack)
-
-		if auth != prompt_fullstack.Auth_None {
-			executors_fullstack.CreateAuth(dir, auth)
-		}
-
-		if orm != utils.Orms_None {
-			executors_fullstack.CreateORM(dir, orm, dbURL, dbType)
-		}
+		var wg sync.WaitGroup
+		wg.Add(3)
+		go func() {
+			defer wg.Done()
+			executors_fullstack.CreateFullStack(dir, fullStack)
+		}()
+		go func() {
+			defer wg.Done()
+			if auth != prompt_fullstack.Auth_None {
+				executors_fullstack.CreateAuth(dir, auth)
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			if orm != utils.Orms_None {
+				executors_fullstack.CreateORM(dir, orm, dbURL, dbType)
+			}
+		}()
+		wg.Wait()
 
 	} else if projType == utils.ProjectTypeMonoRepos {
 		monorepo := prompt_monorepo.AskMonoRepo()
